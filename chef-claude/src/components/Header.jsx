1ef8a5b89837f7465ee0c7e61ca247a107eaeb2f
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-09-24 14:13:02 -0700

    finished header

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="header-icon" src="/src/images/chef-claude-icon.png" alt="chef claude icon"/>
+            <span className="header-text">Chef Claude</span>
+        </header>
+    )
+}
