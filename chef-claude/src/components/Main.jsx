312d172c524eaecc66c69f59834d125f49418682
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-09-25 14:53:51 -0700

    implemented add ingredients basic functionality

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,14 +1,39 @@
+import React from "react";
+
 export default function Main() {
-  return (
-    <main>
-      <form className="add-ingredient-form">
-        <input 
-            type="text"
-            aria-label="Add ingredient"
-            placeholder="e.g. oregano"
-        />
-        <button>+ Add ingredient</button>
-      </form>
-    </main>
-  );
+    const [ingredients, setIngredients] = React.useState([])
+
+    const ingredientsListItems = ingredients.map(
+        (ingredient) => <li key={ingredient}>{ingredient}</li>
+    )
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        if (newIngredient && !ingredients.includes(newIngredient)) {
+            setIngredients(prev => [...prev, newIngredient])
+        }
+        event.currentTarget.reset()
+
+    }
+    return (
+        <main>
+            <form 
+                className="add-ingredient-form"
+                onSubmit={handleSubmit}
+            >
+                <input 
+                    type="text"
+                    aria-label="Add ingredient"
+                    placeholder="e.g. oregano"
+                    name="ingredient"
+                />
+                <button>+ Add ingredient</button>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main> 
+    );
 }
