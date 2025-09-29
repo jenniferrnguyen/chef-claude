2893256868c3731e41d0a40ccb060821690b86d8
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-09-28 20:30:45 -0700

    added max width to ingredients and get recipe

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -16,12 +16,12 @@ export default function Main() {
 
     const ingredientsAndGetRecipe = () => {
         return (
-            <section>
+            <section className="ingredients-and-get-recipe-section">
                 <h2 className="ingredients-header">Ingredients on hand:</h2>
                 <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
                 <div className="get-recipe-container">
                     <div>
-                        <h3>Ready of a recipe?</h3>
+                        <h3>Ready for a recipe?</h3>
                          <p>Generate a recipe from your list of ingredients.</p>
                     </div>
                     <button>Get a recipe</button>
