43b88f3316c2646e3754a5621ec2238153bfbe42
Jennifer Nguyen <jennifer.t.nguyen.02@gmail.com>
2025-09-30 19:47:30 -0700

    refector ingredient's list and claude recipe to their own components

---
+++ chef-claude/src/components/IngredientsList.jsx
@@ -0,0 +1,23 @@
+export default function IngredientsList(props) {
+  const ingredientListItems = props.ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  return (
+    <section className="ingredients-and-get-recipe-section">
+      <h2 className="ingredients-header">Ingredients on hand:</h2>
+      <ul className="ingredients-list" aria-live="polite">
+        {ingredientListItems}
+      </ul>
+      {props.ingredients.length > 3 && (
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+        </div>
+      )}
+    </section>
+  );
+}
